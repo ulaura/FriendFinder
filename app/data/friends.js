c5f0659bf4b4275f410d8f868c823bf050fc4db3
Laura Unaeze <l.unaeze5@gmail.com>
2017-10-20 15:53:52 -0700

    Set up array to hold data in friends.js.

---
+++ app/data/friends.js
@@ -0,0 +1,15 @@
+//The Data
+
+//The array that will store all possible friends,
+//their photos, and their survey results.
+//This will be an array of objects
+var friendArray = [
+	{
+		"name": "",
+		"photo": "",
+		"scores": [
+		]
+	}
+];
+
+module.exports = friendArray;
