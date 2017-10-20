654fca7619551bcf596732cd662b76d172724302
Laura Unaeze <l.unaeze5@gmail.com>
2017-10-20 15:48:24 -0700

    Added GET request and started POST request to api.

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,25 @@
+//Loading data
+
+// linking routes to data sources found in data/friends.js
+var friends = require("../data/friends");
+
+
+//Routing
+module.exports = function(app) {
+	//API GET request. Will display JSON of all possible friends
+	app.get("/api/friends", function(req, res) {
+		res.json(friends);
+	});
+
+	//API POST request. 
+	app.post("/api/friends", function(req, res) {
+		
+	})
+
+
+
+
+
+
+
+}
